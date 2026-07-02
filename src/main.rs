fdc3c3c7081449bd3fb38e68ce145fdc76693673
Frederik Bolding <frederik.bolding@gmail.com>
2026-07-03 00:46:35 +0200

    Remove ProtocolHandler in favor of simpler approach

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use anyhow::{Result, anyhow};
 use blake3::{Hash, Hasher};
 use iroh::{
     Endpoint,
-    endpoint::{Connection, RecvStream, presets},
-    protocol::{AcceptError, ProtocolHandler, Router},
+    endpoint::{RecvStream, presets},
 };
 use iroh_tickets::{Ticket, endpoint::EndpointTicket};
 use std::{env, fs};
@@ -31,7 +30,12 @@ async fn main() -> Result<()> {
 }
 
 async fn send(file: Vec<u8>) -> Result<()> {
-    let endpoint = Endpoint::bind(presets::N0).await?;
+    let hash = blake3_hash(&file);
+
+    let endpoint = Endpoint::builder(presets::N0)
+        .alpns(vec![ALPN.to_vec()])
+        .bind()
+        .await?;
 
     endpoint.online().await;
 
@@ -39,11 +43,35 @@ async fn send(file: Vec<u8>) -> Result<()> {
 
     println!("[bifrost] Use the following ticket to connect {}", ticket);
 
-    let bifrost = Bifrost::new(file);
+    let incoming = endpoint
+        .accept()
+        .await
+        .ok_or_else(|| anyhow!("[bifrost] Incoming connection failed."))?;
 
-    let router = Router::builder(endpoint).accept(ALPN, bifrost).spawn();
+    let connection = incoming.await?;
 
-    tokio::signal::ctrl_c().await?;
+    let (mut send, mut recv) = connection.accept_bi().await?;
+    println!("[bifrost] Connection established!");
+
+    let _request = recv_bytes(&mut recv, 5).await.unwrap();
+
+    send.write_all(hash.as_bytes()).await.unwrap();
+    send.write_all(&file.len().to_be_bytes()).await.unwrap();
+
+    // TODO
+    // Sender: Hash, compress, send
+    // Receiver: Receive, decompress, hash
+    send.write_all(&file).await.unwrap();
+
+    println!("[bifrost] Sent {} bytes", file.len());
+
+    send.finish()?;
+
+    let _response = recv_bytes(&mut recv, 2).await.unwrap();
+
+    connection.close(0u32.into(), b"OK");
+
+    endpoint.close().await;
 
     Ok(())
 }
@@ -88,49 +116,6 @@ async fn receive(ticket: EndpointTicket) -> Result<()> {
     file.write_all(&response).await?;
 
     Ok(())
-}
-#[derive(Debug)]
-struct Bifrost {
-    file: Vec<u8>,
-    hash: Hash,
-}
-
-impl ProtocolHandler for Bifrost {
-    async fn accept(&self, connection: Connection) -> Result<(), AcceptError> {
-        let (mut send, mut recv) = connection.accept_bi().await?;
-        println!("[bifrost] Connection established!");
-
-        let _request = recv_bytes(&mut recv, 5).await.unwrap();
-
-        let file = &self.file;
-
-        send.write_all(self.hash.as_bytes()).await.unwrap();
-        send.write_all(&file.len().to_be_bytes()).await.unwrap();
-
-        // TODO
-        // Sender: Hash, compress, send
-        // Receiver: Receive, decompress, hash
-        send.write_all(&self.file).await.unwrap();
-
-        println!("[bifrost] Sent {} bytes", file.len());
-
-        send.finish()?;
-
-        let _response = recv_bytes(&mut recv, 2).await.unwrap();
-
-        connection.close(0u32.into(), b"OK");
-
-        Ok(())
-    }
-}
-
-impl Bifrost {
-    pub fn new(file: Vec<u8>) -> Self {
-        Self {
-            hash: blake3_hash(&file),
-            file,
-        }
-    }
 }
 
 async fn recv_bytes(stream: &mut RecvStream, length: usize) -> Result<Vec<u8>> {
